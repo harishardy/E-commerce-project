9579eeba028ba0b83228109478a6c5483b2a2019
Harish Hardy <mj.harishardy@gmail.com>
2019-04-11 11:55:44 +0530

    UserDetail.java,(deleted AppTest.java)

---
+++ BlueChipBackend/src/main/java/com/ecomm/model/UserDetail.java
@@ -0,0 +1,65 @@
+package com.ecomm.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class UserDetail 
+{
+	@Id
+	String username;
+	String password;
+	boolean enabled;
+	String role;
+	String address;
+	String emailId;
+	String mobileNo;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getMobileNo() {
+		return mobileNo;
+	}
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+	
+	
+}
