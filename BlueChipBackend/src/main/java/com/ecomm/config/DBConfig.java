a6fc9dd7d0e26836ccfaed7d7a3a22f5b7377829
Harish Hardy <mj.harishardy@gmail.com>
2019-04-11 10:06:58 +0530

    DBConfig

---
+++ BlueChipBackend/src/main/java/com/ecomm/config/DBConfig.java
@@ -0,0 +1,72 @@
+package com.ecomm.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan("com.ecomm")
+@EnableTransactionManagement
+
+public class DBConfig 
+{
+	
+	//DataSource Bean
+	
+	@Bean(name="dataSource")
+	public DataSource getH2DataSource()
+	{
+		DriverManagerDataSource dataSource=new DriverManagerDataSource();
+		
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:tcp://localhost/~/Ecommerce");
+		dataSource.setUsername("ecomm");
+		dataSource.setPassword("ecomm");
+		
+		System.out.println("---DataSource Created---");
+		return dataSource;
+	}
+	
+	
+	//SessionFactory Bean
+	
+	@Bean(name="sessionFactory")
+	public SessionFactory getSessionFactory()
+	{
+		
+		Properties hibernateProperties=new Properties();
+		hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
+		hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		
+		
+		
+		LocalSessionFactoryBuilder factory=new LocalSessionFactoryBuilder(getH2DataSource());
+		factory.addProperties(hibernateProperties);
+		
+		System.out.println("---Session Factory Object Created---");
+		return factory.buildSessionFactory();
+		
+		
+		
+	}
+	
+	
+	//HibernateTransaction Bean
+	
+	@Bean(name="txManager")
+	public HibernateTransactionManager getHibernateTransactionManager(SessionFactory sessionFactory)
+	{
+		System.out.println("---Transaction Object---");
+		return new HibernateTransactionManager(sessionFactory);
+	}
+	
+}
