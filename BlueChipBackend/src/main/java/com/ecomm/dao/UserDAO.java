b6ac6efcc0b53a0459cda94a52f2d85ce2028b19
Harish Hardy <mj.harishardy@gmail.com>
2019-04-11 14:34:51 +0530

    UserDAO.java

---
+++ BlueChipBackend/src/main/java/com/ecomm/dao/UserDAO.java
@@ -0,0 +1,10 @@
+package com.ecomm.dao;
+
+import com.ecomm.model.UserDetail;
+
+public interface UserDAO 
+{
+	public boolean registerUser(UserDetail user);
+	public UserDetail getUserDetail(String username);
+	public boolean updateUser(UserDetail user);
+}
