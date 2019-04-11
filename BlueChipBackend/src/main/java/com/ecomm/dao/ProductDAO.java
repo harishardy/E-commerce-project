dfd028016e17e064cac0609dbd08175aba07368b
Harish Hardy <mj.harishardy@gmail.com>
2019-04-11 14:28:10 +0530

    ProductDAOImpl.java, (correction in ProductDAO.java)

--- BlueChipBackend/src/main/java/com/ecomm/dao/ProductDAO.java
+++ BlueChipBackend/src/main/java/com/ecomm/dao/ProductDAO.java
@@ -9,7 +9,7 @@ public interface ProductDAO
 	public boolean addProduct(Product product);
 	public boolean deleteProduct(Product product);
 	public boolean updateProduct(Product product);
-	public Product getProduct();
+	public Product getProduct(int productId);
 	public List<Product> listProducts();
 	public List<Product> listProductCategoryWise(int categoryId);
 }
