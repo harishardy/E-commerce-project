c65f16faa90e921f70383f8b80a9893bce0d32d7
Harish Hardy <mj.harishardy@gmail.com>
2019-04-12 17:41:36 +0530

    header,page controller corrections

--- BlueChipFrontend/src/main/java/com/ecomm/controller/PageController.java
+++ BlueChipFrontend/src/main/java/com/ecomm/controller/PageController.java
@@ -1,21 +1,22 @@
 package com.ecomm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController 
 {
 	@RequestMapping(value="/login")
-	public String showLoginPage()
+	public String showLoginPage(Model m)
 	{
-		System.out.println("I am in Login mapping method");
+		m.addAttribute("title", "Login Page");
 		return "Login";
 	}
 	@RequestMapping(value="/register")
-	public String showRegisterPage()
+	public String showRegisterPage(Model m)
 	{
-		System.out.println("I am in Register mapping method");
+		m.addAttribute("title", "Register Page");
 		return "Register";
 	}
 }
--- BlueChipFrontend/src/main/webapp/WEB-INF/views/Header.jsp
+++ BlueChipFrontend/src/main/webapp/WEB-INF/views/Header.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <title>BlueChip-$(title)</title>
@@ -8,7 +8,7 @@
 <body>
 
 <a href="<c:url value='/login'/>">Login</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-<a href="<register">Register</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="<c:url value="/register"/>">Register</a>&nbsp;&nbsp;|&nbsp;&nbsp;
 <a href="<c:url value='/category'/>">Category</a>
 
 </body>
